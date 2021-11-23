bd710b04cd84b1190f245b2e70e97705b21534f6
eadame42 <93883381+eadame42@users.noreply.github.com>
2021-11-23 15:14:20 -0600

    Final Version - Imagination

--- homepage/Untitled-1.js
+++ homepage/Untitled-1.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const registerVideo = (bound, video) => {
 	bound = document.querySelector(bound);
 	video = document.querySelector(video);
@@ -19,4 +20,27 @@ registerVideo("#bound-one", "#bound-one video");
 
 registerVideo("#bound-two", "#bound-two video")
 
+=======
+const registerVideo = (bound, video) => {
+	bound = document.querySelector(bound);
+	video = document.querySelector(video);
+	const scrollVideo = ()=>{
+		if(video.duration) {
+			const distanceFromTop = window.scrollY + bound.getBoundingClientRect().top;
+			const rawPercentScrolled = (window.scrollY - distanceFromTop) / (bound.scrollHeight - window.innerHeight);
+			const percentScrolled = Math.min(Math.max(rawPercentScrolled, 0), 1);
+			
+			video.currentTime = video.duration * percentScrolled;
+		}
+		requestAnimationFrame(scrollVideo);
+	}
+	requestAnimationFrame(scrollVideo);
+}
+
+
+registerVideo("#bound-one", "#bound-one video");
+
+registerVideo("#bound-two", "#bound-two video")
+
+>>>>>>> d63a5d220ca46ea794b93a2d93069b0231ca27c8
 registerVideo("#bound-three", "#bound-three video")
